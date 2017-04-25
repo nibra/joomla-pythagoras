aa891104a306d8259f9b62fd201e4f54b951954b
Niels Braczek <nbraczek@bsds.de>
2017-04-26 00:05:20 +0200

    Refactored HtmlRenderer to use ContentTypeVisitorTrait

--- libraries/incubator/Renderer/HtmlRenderer.php
+++ libraries/incubator/Renderer/HtmlRenderer.php
@@ -11,11 +11,9 @@ namespace Joomla\Renderer;
 use Joomla\Cms\Entity\Menu;
 use Joomla\Content\CompoundTypeInterface;
 use Joomla\Content\ContentTypeInterface;
-use Joomla\Content\Type\Accordion;
+use Joomla\Content\ContentTypeVisitorTrait;
 use Joomla\Content\Type\Article;
 use Joomla\Content\Type\Attribution;
-use Joomla\Content\Type\Columns;
-use Joomla\Content\Type\Compound;
 use Joomla\Content\Type\DataTable;
 use Joomla\Content\Type\DefaultMenu;
 use Joomla\Content\Type\Dump;
@@ -59,25 +57,13 @@ class HtmlRenderer extends Renderer implements JavascriptAwareInterface, CssAwar
 	/** @var string  Layout directory */
 	protected $layoutDirectory = 'bootstrap-3';
 
-	/** @var  ScriptStrategyInterface */
-	private $clientScript;
-
 	/** @var  LayoutFactory */
 	private $layoutFactory;
 
 	use DumpTrait;
 	use JavascriptAwareImplementation;
 	use CssAwareImplementation;
-
-	/**
-	 * @param   ScriptStrategyInterface $strategy The scripting strategy (library) to use
-	 *
-	 * @return  void
-	 */
-	public function setScriptStrategy(ScriptStrategyInterface $strategy)
-	{
-		$this->clientScript = $strategy;
-	}
+	use ContentTypeVisitorTrait;
 
 	/**
 	 * Sets the template
@@ -96,88 +82,60 @@ class HtmlRenderer extends Renderer implements JavascriptAwareInterface, CssAwar
 	}
 
 	/**
-	 * Render an accordion
+	 * Common handler for different ContentTypes.
 	 *
-	 * @param   Accordion $accordion The accordion
+	 * @param string               $method  The name of the originally called method
+	 * @param ContentTypeInterface $content The content
 	 *
-	 * @return  void
+	 * @return void
 	 */
-	public function visitAccordion(Accordion $accordion)
+	public function visit($method, $content)
 	{
-		$this->preRenderChildElements($accordion);
-
-		$this->applyLayout('Accordion', $accordion);
+		$contentType = str_replace('visit', '', $method);
+		$this->preRenderChildElements($content);
+		$this->applyLayout($contentType, $content);
 	}
 
 	/**
-	 * Render an article
+	 * Pre-render child elements.
 	 *
-	 * @param   Article $article The article
+	 * This is usually only needed in plain text layouts.
+	 *
+	 * @param   ContentTypeInterface $content The content element
 	 *
 	 * @return  void
+	 * @todo    Move this method to LayoutWrapper
 	 */
-	public function visitArticle(Article $article)
+	private function preRenderChildElements(ContentTypeInterface $content)
 	{
-		$this->applyLayout('Article', $article);
+		if (!($content instanceof CompoundTypeInterface))
+		{
+			return;
+		}
+
+		$stash = $this->output;
+
+		foreach ($content->getChildren() as $key => $item)
+		{
+			$this->output = '';
+			$item->accept($this);
+			$item->html = $this->output;
+		}
+
+		$this->output = $stash;
 	}
 
 	/**
-	 * Render an attribution to an author
+	 * Apply a layout
 	 *
-	 * @param   Attribution $attribution The attribution
-	 *
-	 * @return  void
-	 */
-	public function visitAttribution(Attribution $attribution)
-	{
-		$this->applyLayout('Attribution', $attribution);
-	}
-
-	/**
-	 * Render columns
-	 *
-	 * @param   Columns $columns The columns
+	 * @param   string                      $contentType The filename of the layout file
+	 * @param   object|ContentTypeInterface $content     The content
 	 *
 	 * @return  void
 	 */
-	public function visitColumns(Columns $columns)
+	private function applyLayout($contentType, $content)
 	{
-		$this->preRenderChildElements($columns);
-
-		$this->applyLayout('Columns', $columns);
-	}
-
-	/**
-	 * Render a compound (block) element
-	 *
-	 * @param   Compound $compound The compound
-	 *
-	 * @return  void
-	 */
-	public function visitCompound(Compound $compound)
-	{
-		$this->applyLayout('Compound', $compound);
-	}
-
-	/**
-	 * Render a data table
-	 *
-	 * @param   DataTable $dataTable The data table
-	 *
-	 * @return  void
-	 */
-	public function visitDataTable(DataTable $dataTable)
-	{
-		$items = $dataTable->getChildren();
-
-		/** @var EntityBuilder $entityBuilder */
-		$entityBuilder = $this->container->get('Repository')->getEntityBuilder();
-
-		$params           = $dataTable->getParameters();
-		$params['entity'] = $entityBuilder->getMeta(get_class($items[0]->item));
-		$dataTable->setParameters($params);
-
-		$this->applyLayout('DataTable', $dataTable);
+		$this->layoutFactory->createLayout($contentType, $content)->render($this);
 	}
 
 	/**
@@ -218,6 +176,39 @@ class HtmlRenderer extends Renderer implements JavascriptAwareInterface, CssAwar
 	}
 
 	/**
+	 * @param   string $url  The URL
+	 * @param   Page   $page The page
+	 *
+	 * @return string
+	 */
+	private function expandUrl($url, $page)
+	{
+		if (empty($url))
+		{
+			return '/index.php';
+		}
+
+		while ($url[0] != '/' && !empty($page->parent))
+		{
+			// @todo refactor
+			if ($page->parent instanceof Layout)
+			{
+				break;
+			}
+
+			$page = $page->parent;
+			$url  = $page->url . '/' . $url;
+		}
+
+		if ($url[0] != '/')
+		{
+			$url = '/' . $url;
+		}
+
+		return '/index.php' . $url;
+	}
+
+	/**
 	 * Dump an item
 	 *
 	 * @param   Dump $dump The dump
@@ -227,197 +218,6 @@ class HtmlRenderer extends Renderer implements JavascriptAwareInterface, CssAwar
 	public function visitDump(Dump $dump)
 	{
 		$this->write('<pre>' . $this->dumpEntity($dump->item) . '</pre>');
-	}
-
-	/**
-	 * Render a headline.
-	 *
-	 * @param   Headline $headline The headline
-	 *
-	 * @return  void
-	 */
-	public function visitHeadline(Headline $headline)
-	{
-		$this->applyLayout('Headline', $headline);
-	}
-
-	/**
-	 * Apply a layout
-	 *
-	 * @param   string                      $contentType The filename of the layout file
-	 * @param   object|ContentTypeInterface $content     The content
-	 *
-	 * @return  void
-	 */
-	private function applyLayout($contentType, $content)
-	{
-		$this->layoutFactory->createLayout($contentType, $content)->render($this);
-	}
-
-	/**
-	 * Render a horizontal line.
-	 *
-	 * @param   HorizontalLine $headline The horizontal line
-	 *
-	 * @return  void
-	 */
-	public function visitHorizontalLine(HorizontalLine $headline)
-	{
-		$this->write("<hr>\n");
-	}
-
-	/**
-	 * Render an icon
-	 *
-	 * @param   Icon $icon The icon
-	 *
-	 * @return  void
-	 */
-	public function visitIcon(Icon $icon)
-	{
-		$this->applyLayout('Icon', $icon);
-	}
-
-	/**
-	 * Render an image
-	 *
-	 * @param   Image $image The image
-	 *
-	 * @return  void
-	 */
-	public function visitImage(Image $image)
-	{
-		$this->applyLayout('Image', $image);
-	}
-
-	/**
-	 * Render a link
-	 *
-	 * @param Link $link
-	 *
-	 * @return  void
-	 */
-	public function visitLink(Link $link)
-	{
-		$this->applyLayout('Link', $link);
-	}
-
-	/**
-	 * Render an OnePager
-	 *
-	 * @param   OnePager $page The page
-	 *
-	 * @return  void
-	 */
-	public function visitOnePager(OnePager $page)
-	{
-		$this->preRenderChildElements($page);
-
-		$this->applyLayout('Onepager', $page);
-	}
-
-	/**
-	 * Render an OnePager section
-	 *
-	 * @param   OnePagerSection $section The page
-	 *
-	 * @return  void
-	 */
-	public function visitOnePagerSection(OnePagerSection $section)
-	{
-		$this->preRenderChildElements($section);
-
-		$this->applyLayout('OnepagerSection', $section);
-	}
-
-	/**
-	 * Render a paragraph
-	 *
-	 * @param   Paragraph $paragraph The paragraph
-	 *
-	 * @return  void
-	 */
-	public function visitParagraph(Paragraph $paragraph)
-	{
-		$this->applyLayout('Paragraph', $paragraph);
-	}
-
-	/**
-	 * Render rows
-	 *
-	 * @param   Rows $rows The rows
-	 *
-	 * @return  void
-	 */
-	public function visitRows(Rows $rows)
-	{
-		$this->preRenderChildElements($rows);
-
-		$this->applyLayout('Rows', $rows);
-	}
-
-	/**
-	 * Render an slider
-	 *
-	 * @param   Slider $slider The slider
-	 *
-	 * @return  void
-	 */
-	public function visitSlider(Slider $slider)
-	{
-		$this->preRenderChildElements($slider);
-
-		$this->applyLayout('Slider', $slider);
-	}
-
-	/**
-	 * @param   ContentTypeInterface $content The content element
-	 *
-	 * @return  void
-	 */
-	private function preRenderChildElements(ContentTypeInterface $content)
-	{
-		if (!($content instanceof CompoundTypeInterface))
-		{
-			return;
-		}
-
-		$stash = $this->output;
-
-		foreach ($content->getChildren() as $key => $item)
-		{
-			$this->output = '';
-			$item->accept($this);
-			$item->html = $this->output;
-		}
-
-		$this->output = $stash;
-	}
-
-	/**
-	 * Render a span element
-	 *
-	 * @param   Span $span The text
-	 *
-	 * @return  void
-	 */
-	public function visitSpan(Span $span)
-	{
-		$this->applyLayout('Span', $span);
-	}
-
-	/**
-	 * Render tabs
-	 *
-	 * @param   Tabs $tabs The tabs
-	 *
-	 * @return  void
-	 */
-	public function visitTabs(Tabs $tabs)
-	{
-		$this->preRenderChildElements($tabs);
-
-		$this->applyLayout('Tabs', $tabs);
 	}
 
 	/**
@@ -471,49 +271,23 @@ class HtmlRenderer extends Renderer implements JavascriptAwareInterface, CssAwar
 	}
 
 	/**
-	 * @param   string $url  The URL
-	 * @param   Page   $page The page
+	 * Render a data table
 	 *
-	 * @return string
-	 */
-	private function expandUrl($url, $page)
-	{
-		if (empty($url))
-		{
-			return '/index.php';
-		}
-
-		while ($url[0] != '/' && !empty($page->parent))
-		{
-			// @todo refactor
-			if ($page->parent instanceof Layout)
-			{
-				break;
-			}
-
-			$page = $page->parent;
-			$url  = $page->url . '/' . $url;
-		}
-
-		if ($url[0] != '/')
-		{
-			$url = '/' . $url;
-		}
-
-		return '/index.php' . $url;
-	}
-
-	/**
-	 * Render a tree
-	 *
-	 * @param   Tree $tree The tree
+	 * @param   DataTable $dataTable The data table
 	 *
 	 * @return  void
 	 */
-	public function visitTree(Tree $tree)
+	public function visitDataTable(DataTable $dataTable)
 	{
-		$this->preRenderChildElements($tree);
+		$items = $dataTable->getChildren();
 
-		$this->applyLayout('Tree', $tree);
+		/** @var EntityBuilder $entityBuilder */
+		$entityBuilder = $this->container->get('Repository')->getEntityBuilder();
+
+		$params           = $dataTable->getParameters();
+		$params['entity'] = $entityBuilder->getMeta(get_class($items[0]->item));
+		$dataTable->setParameters($params);
+
+		$this->applyLayout('DataTable', $dataTable);
 	}
 }
